5952cedc3e6d2143b9269467c35e1f158c978997
Sanket Marathe <sanketmarathe2001@gmail.com>
2021-09-01 12:06:34 +0530

    Create datatype.cpp
    Datatypes

---
+++ datatype.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+//int datatype accepts integer values.  
+int i=10;
+
+// both float and double datatype accept floating-point numbers.
+float f=10.5;
+double d=12.0896;
+
+//boolean datatype accept true/false.
+bool b = false;
+
+//char datatype accept single character value
+char c = 'A';
+
+//string datatype accept sentence
+string s="hello";
+
+cout<<i<<"\n";
+cout<<f<<"\n";
+cout<<d<<"\n";
+
+//boolean datatype display 1 for true and 0 for false. 
+cout<<b<<"\n";
+
+cout<<c<<"\n";
+cout<<s<<"\n";
+
+
+}
