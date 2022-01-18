689a6098bd332221bd3e04c9519a003e20f9160b
Sanket Marathe <sanketmarathe2001@gmail.com>
2022-01-18 19:26:41 +0530

    Update input_taking_for_graph.cpp

--- Graph & Trees/input_taking_for_graph.cpp
+++ Graph & Trees/input_taking_for_graph.cpp
@@ -9,13 +9,14 @@ vector<int> graph2[N];
 int main(){
 	int n,m;
 	cin>>n>>m;
-	//Adjacency Matrix
 	for(int i=0;i<m;i++){
 		int v1,v2;
 		cin>>v1>>v2;
+		//Adjacency Matrix
 		graph[v1][v2]=1;
 		graph[v2][v1]=1;
 
+		//Adjacency List
 		graph2[v1].push_back(v2);
 		graph2[v2].push_back(v1);
 	}
